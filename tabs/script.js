f68be295b9725db78fc85d3f35d1a175df5567ef
Raj Kashyap <rajk917958@gmai.com>
2023-11-13 20:50:08 +0530

    Removed repeatation and optimized code

--- tabs/script.js
+++ tabs/script.js
@@ -1,41 +1,32 @@
 const tabButtons = document.querySelectorAll(".tab-btn");
 const tabContents = document.querySelectorAll(".content");
 const tabLine = document.querySelector(".line");
+tabLine.style.left = `${tabButtons[0].offsetLeft}px`;
+tabLine.style.width = `${tabButtons[0].offsetWidth}px`;
 
-tabButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    switch (button.id) {
-      case "home-btn":
-        tabContents[0].style.transform = "translateX(0)";
-        tabContents[1].style.transform = "translateX(100%)";
-        tabContents[2].style.transform = "translateX(100%)";
-        tabButtons[0].style.color = "#2c4af1";
-        tabButtons[1].style.color = "black";
-        tabButtons[2].style.color = "black";
-        tabLine.style.left = `${button.offsetLeft}px`;
-        tabLine.style.width = `${button.offsetWidth}px`;
-        break;
-      case "about-btn":
-        tabContents[0].style.transform = "translateX(100%)";
-        tabContents[1].style.transform = "translateX(0)";
-        tabContents[2].style.transform = "translateX(100%)";
-        tabButtons[0].style.color = "black";
-        tabButtons[1].style.color = "#2c4af1";
-        tabButtons[2].style.color = "black";
-        tabLine.style.left = `${button.offsetLeft}px`;
-        tabLine.style.width = `${button.offsetWidth}px`;
-        break;
-      case "more-btn":
-        tabContents[0].style.transform = "translateX(100%)";
-        tabContents[1].style.transform = "translateX(100%)";
-        tabContents[2].style.transform = "translateX(0)";
-        tabButtons[0].style.color = "black";
-        tabButtons[1].style.color = "black";
-        tabButtons[2].style.color = "#2c4af1";
-        tabLine.style.left = `${button.offsetLeft}px`;
-        tabLine.style.width = `${button.offsetWidth}px`;
-      default:
-        break;
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  const tabButtons = document.querySelectorAll(".tab-btn");
+  const tabContents = document.querySelectorAll(".content");
+  const tabLine = document.querySelector(".line");
+
+  tabButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      tabContents.forEach((content, contentIndex) => {
+        const isActive = contentIndex === index;
+
+        content.style.transform = isActive
+          ? "translateX(0)"
+          : "translateX(100%)";
+        content.style.opacity = isActive ? "1" : "0"; // Show selected content with opacity transition
+
+        if (isActive) {
+          button.style.color = "#2c4af1";
+          tabLine.style.left = `${button.offsetLeft}px`;
+          tabLine.style.width = `${button.offsetWidth}px`;
+        } else {
+          button.style.color = "black";
+        }
+      });
+    });
   });
 });
